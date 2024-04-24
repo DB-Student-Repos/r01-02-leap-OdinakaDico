0f8962c36fa5efb84321632699316bcb7c1684a0
OdinakaDico <149073751+OdinakaDico@users.noreply.github.com>
2024-04-24 20:15:44 +0200

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -3,21 +3,3 @@ use std::io;
 pub fn is_leap_year(year: i32) -> bool {
         (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)
 }
-
-fn main() -> Result<(), io::Error> {
-        println!("Enter a year:");
-
-        let mut year_string = String::new();
-        io::stdin().read_line(&mut year_string)?;
-
-        let year: i32 = year_string.trim().parse()
-            .expect("Please type a number!");
-
-        if is_leap_year(year) {
-            println!("{} is a leap year.", year);
-        } else {
-            println!("{} is not a leap year.", year);
-        }
-
-        Ok(())
-}
