4e0b96e833d53204fe3d9d48c662948e6c179b6e
OdinakaDico <149073751+OdinakaDico@users.noreply.github.com>
2024-04-24 20:08:02 +0200

    Update lib.rs file

--- src/lib.rs
+++ src/lib.rs
@@ -4,12 +4,11 @@ pub fn is_leap_year(year: i32) -> bool {
         (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)
 }
 
-fn main() {
+fn main() -> Result<(), io::Error> {
         println!("Enter a year:");
 
         let mut year_string = String::new();
-        io::stdin().read_line(&mut year_string)
-            .expect("Failed to read line");
+        io::stdin().read_line(&mut year_string)?;
 
         let year: i32 = year_string.trim().parse()
             .expect("Please type a number!");
@@ -19,4 +18,6 @@ fn main() {
         } else {
             println!("{} is not a leap year.", year);
         }
+
+        Ok(())
 }
