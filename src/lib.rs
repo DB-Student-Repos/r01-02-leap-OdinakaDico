ac8a20f5319c6fceccd26c1d8bb662a8bea70023
OdinakaDico <149073751+OdinakaDico@users.noreply.github.com>
2024-04-24 19:29:28 +0200

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,22 @@
-pub fn is_leap_year(year: u64) -> bool {
-    unimplemented!("true if {year} is a leap year")
+use std::io;
+
+fn main() {
+        println!("Enter a year:");
+
+        let mut year_string = String::new();
+        io::stdin().read_line(&mut year_string)
+            .expect("Failed to read line");
+
+        let year: i32 = year_string.trim().parse()
+            .expect("Please type a number!");
+
+        if is_leap_year(year) {
+            println!("{} is a leap year.", year);
+        } else {
+            println!("{} is not a leap year.", year);
+        }
+}
+
+fn is_leap_year(year: i32) -> bool {
+        (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)
 }
